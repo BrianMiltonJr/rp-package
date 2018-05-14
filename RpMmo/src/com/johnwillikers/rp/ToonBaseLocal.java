43ce1731a682d394ac3f8cb396fa953dcd7bafe8
Harlan Milton <harlanbmiltonjr@gmail.com>
2018-05-14 05:56:36 -0500

    Fixed Dir variable and made the file name the players UUID not ID

--- RpMmo/src/com/johnwillikers/rp/ToonBaseLocal.java
+++ RpMmo/src/com/johnwillikers/rp/ToonBaseLocal.java
@@ -11,9 +11,11 @@ import java.sql.SQLException;
 
 import org.json.JSONObject;
 
+import com.johnwillikers.rp.enums.Codes;
+
 public class ToonBaseLocal {
 	
-	public static String dir = "ToonBase/";
+	public static String dir = Mmo.dir + "ToonBase/";
 	
 	
 	public static void storeToon(int playerId, String playerUuid) {
@@ -67,18 +69,18 @@ public class ToonBaseLocal {
 		});
 	}
 	
-	public static void uploadToon(JSONObject toonDataJson, int playerId) {
+	public static void uploadToon(JSONObject toonDataJson) {
 		final int[] toonData = getToonDataIntArray(toonDataJson);
-		final String toonsQuery = "INSERT INTO toons SET xp = " + toonData[0] + ", level = " + toonData[1] + ", stat_points = " + toonData[2] + ", skill_points = "  + toonData[3] + " WHERE toon_id=" + playerId + ";";
-		final String statsQuery = "INSERT INTO stats SET strength = " + toonData[4] + ", agility = " + toonData[5] + ", dexterity = " + toonData[6] + ", constitution = " + toonData[7] + ", spirit = " + toonData[8] + " WHERE toon_id=" + toonData[15] + ";";
-		final String skillsQuery = "INSERT INTO skills SET sword = " + toonData[9] + ", shield = " + toonData[10] + ", axe = " + toonData[11] + ", bow = " + toonData[12] + ", light_armor = " + toonData[13] + ", heavy_armor = " + toonData[14] + " WHERE toon_id=" + toonData[15]+ ";";
+		final String toonsQuery = "UPDATE toons SET xp = " + toonData[0] + ", level = " + toonData[1] + ", stat_points = " + toonData[2] + ", skill_points = "  + toonData[3] + " WHERE id=" + toonData[15] + ";";
+		final String statsQuery = "UPDATE stats SET strength = " + toonData[4] + ", agility = " + toonData[5] + ", dexterity = " + toonData[6] + ", constitution = " + toonData[7] + ", spirit = " + toonData[8] + " WHERE toon_id=" + toonData[15] + ";";
+		final String skillsQuery = "UPDATE skills SET sword = " + toonData[9] + ", shield = " + toonData[10] + ", axe = " + toonData[11] + ", bow = " + toonData[12] + ", light_armor = " + toonData[13] + ", heavy_armor = " + toonData[14] + " WHERE toon_id=" + toonData[15]+ ";";
 		DbHandler.executeUpdate(toonsQuery, Mmo.name);
 		DbHandler.executeUpdate(statsQuery, Mmo.name);
 		DbHandler.executeUpdate(skillsQuery, Mmo.name);
 	}
 	
-	public static void deleteToon(int playerId) {
-		File toonFile = new File(ToonBaseLocal.dir + playerId + ".json");
+	public static void deleteToon(String playerUuid) {
+		File toonFile = new File(dir + playerUuid + ".json");
 		if(toonFile.exists())
 			toonFile.delete();
 	}
@@ -91,7 +93,7 @@ public class ToonBaseLocal {
 	}
 	
 	public static JSONObject readToon(String playerUuid) {
-		File toonFile = new File(ToonBaseLocal.dir + playerUuid + ".json");
+		File toonFile = new File(dir + playerUuid + ".json");
 		if(!toonFile.exists())
 			return null;
 		try {
@@ -111,18 +113,10 @@ public class ToonBaseLocal {
 	public static void writeToon(int[] toon, int[] stats, int[] skills, String playerUuid) {
 		JSONObject toonData = new JSONObject();
 		toonData.put("toon_id", toon[0]);
-		if(toon.length == 5) {
-			toonData.put("xp", toon[2]);
-			toonData.put("level", toon[3]);
-			toonData.put("stat_points", toon[4]);
-			toonData.put("skill_points", toon[5]);
-		}
-		if(toon.length == 4) {
-			toonData.put("xp", toon[0]);
-			toonData.put("level", toon[1]);
-			toonData.put("stat_points", toon[2]);
-			toonData.put("skill_points", toon[3]);
-		}
+		toonData.put("xp", toon[2]);
+		toonData.put("level", toon[3]);
+		toonData.put("stat_points", toon[4]);
+		toonData.put("skill_points", toon[5]);
 		toonData.put("strength", stats[0]);
 		toonData.put("agility", stats[1]);
 		toonData.put("dexterity", stats[2]);
@@ -134,7 +128,11 @@ public class ToonBaseLocal {
 		toonData.put("bow", skills[3]);
 		toonData.put("light_armor", skills[4]);
 		toonData.put("heavy_armor", skills[5]);
-		File toonFile = new File(ToonBaseLocal.dir + playerUuid + ".json");
+		updateToon(toonData, playerUuid);
+	}
+	
+	public static void updateToon(JSONObject toonData, String playerUuid) {
+		File toonFile = new File(dir + playerUuid + ".json");
 		if(toonFile.exists())
 			toonFile.delete();
 		try {
@@ -148,7 +146,10 @@ public class ToonBaseLocal {
 	
 	public static void createPath() {
 		File dir = new File(ToonBaseLocal.dir);
-		if(!dir.exists())
+		Core.debug(Mmo.name, "ToonBaseLocal.createPath", "Dir = " + dir.toString());
+		if(!dir.exists()) {
 			dir.mkdirs();
+			Core.log(Mmo.name, Codes.FIRST_LAUNCH.toString(), "Created " + dir.toString());
+		}
 	}
 }
