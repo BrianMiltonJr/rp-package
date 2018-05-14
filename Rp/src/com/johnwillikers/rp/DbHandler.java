b9aec3398f60f5b9dc1782349f7237011a55d313
Harlan Milton <harlanbmiltonjr@gmail.com>
2018-05-13 22:33:26 -0500

    Moved the creation of Rp_Karma's MySql Db Tables
    Creation of Rp_Karma's tables are created if Rp_Karma is loaded in the
    DbHandler.createTables() command.

--- Rp/src/com/johnwillikers/rp/DbHandler.java
+++ Rp/src/com/johnwillikers/rp/DbHandler.java
@@ -13,7 +13,15 @@ public class DbHandler {
 	
 	public static void createTables() {
 		String query = "CREATE TABLE IF NOT EXISTS `" + Core.db + "`.`players` ( `id` INT NOT NULL AUTO_INCREMENT , `uuid` VARCHAR(200) NOT NULL , `first` VARCHAR(200) NOT NULL , `last` VARCHAR(200) NOT NULL , `player_name` VARCHAR(200) NOT NULL , `gender` INT NOT NULL , `creation_ip` VARCHAR(200) NOT NULL , `last_ip` VARCHAR(200) NOT NULL , `created_at` VARCHAR(200) NOT NULL , `updated_at` VARCHAR(200) NOT NULL , PRIMARY KEY (`id`)) ENGINE = MyISAM";
+		String gamemastersTableQuery = "CREATE TABLE IF NOT EXISTS `" + Core.db +"`.`gamemasters` ( `id` INT NOT NULL AUTO_INCREMENT , `uuid` VARCHAR(200) NOT NULL , `name` VARCHAR(200) NOT NULL , PRIMARY KEY (`id`)) ENGINE = MyISAM;";
+		String karmaTableQuery = "CREATE TABLE IF NOT EXISTS `" + Core.db + "`.`karma` ( `id` INT NOT NULL AUTO_INCREMENT , `player_id` INT NOT NULL , `karma` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = MyISAM;";
+		String reportsTableQuery = "CREATE TABLE IF NOT EXISTS `" + Core.db + "`.`reports` ( `id` INT NOT NULL AUTO_INCREMENT , `gm_id` INT NOT NULL , `player_id` INT NOT NULL , `body` VARCHAR(200) NOT NULL , `actions` INT NOT NULL , `date` VARCHAR(200) NOT NULL , PRIMARY KEY (`id`)) ENGINE = MyISAM;";
 		executeUpdate(query, Core.name);
+		if(Core.dependables[1] == 1) {
+			executeUpdate(gamemastersTableQuery, Core.name);
+			executeUpdate(karmaTableQuery, Core.name);
+			executeUpdate(reportsTableQuery, Core.name);
+		}
 	}
 	
 	public static void executeQuery(Plugin plugin, String query, String pluginName, MySqlCallback callback) {
